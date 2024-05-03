6187585b18091363bd5fe79d0901a275ca42201d
Adam <adam.nemmish213@gmail.com>
2024-05-03 22:37:24 +0600

    Продолжение проекта 52

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from webapp.models import Tasks
+
+
+@admin.register(Tasks)
+class TasksAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_done']
+    list_filter = ['status']
+    search_fields = ['status', 'id']
+    fields = ['description', 'status', 'date_done']
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from webapp.models import Tasks, status_choices
+
+
+def task_page(request):
+    tasks = Tasks.objects.all()
+    return render(request, 'task_page.html', {'tasks': tasks})
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def task_create(request):
+    if request.method == 'GET':
+        return render(request, 'task_create.html', {'status': status_choices})
+    elif request.method == "POST":
+        date_done = request.POST.get('date_done')
+        if not date_done:
+            date_done = None
+        Tasks.objects.create(
+            description=request.POST.get('description'),
+            status=request.POST.get('status'),
+            date_done=date_done
+        )
+        return HttpResponseRedirect('/task_page')
+
